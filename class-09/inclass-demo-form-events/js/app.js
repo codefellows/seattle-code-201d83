762cc6cf4e2c5d83888a1e77e5e11c1cfd77fafc
Audrey Patterson <audrey.patterson31@gmail.com>
2022-04-13 18:45:51 -0700

    Update app.js

--- class-09/inclass-demo-form-events/js/app.js
+++ class-09/inclass-demo-form-events/js/app.js
@@ -4,6 +4,38 @@
 // Step one - grab the element to listen to
 let myform = document.getElementById('my-form');
 
+// Step 1: Grab the element that we want to listen to - for the boxes demo
+//let myContainer = document.getElementById('container');
+//let parentEl = document.getElementById('results');
+
+
+// Step 3: Define our event handler - callback function
+// function handleClick(event) {
+//   console.log('this is the event', event);
+//   console.log('this is the target', event.target);
+
+//   if (event.target.id === 'box-one') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 1 was clicked!';
+//     parentEl.append(pEl);
+//   } else if (event.target.id === 'box-two') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 2 was clicked!';
+//     parentEl.append(pEl);
+//   } else if (event.target.id === 'box-three') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 3 was clicked!';
+//     parentEl.append(pEl);
+//   } else {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'CLICK THE BOXES ONLY!';
+//     parentEl.append(pEl);
+//   }
+// }
+
+// Step 2: Add the Event Listener
+// myContainer.addEventListener('click', handleClick);
+
 
 // Step 3 - event handler
 
